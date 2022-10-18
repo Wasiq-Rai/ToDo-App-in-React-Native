6e19f5520e3b1e160a11d7204f71b45acd019fa2
Wasiq-Rai <wasiqrai1122@gmail.com>
2022-10-18 11:58:10 +0500

    ToDo App Without delete and checkbox functionalities

--- App.js
+++ App.js
@@ -1,20 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,FlatList, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import TaskCard from './TaslCard';
 
 export default function App() {
+ 
+ const  [task,setTask] =useState();
+const [taskArray,setTasArray]=useState([]);
+const [key,setKey]= useState(0);
+
+
+ const addTask=()=>{
+  
+  Keyboard.dismiss();
+  setTasArray([...taskArray,task])
+  setKey(key+1);
+  console.log(task);
+  setTask(null)
+ }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+      <View style={styles.tasksArea}>
+        <Text style={styles.headerText}>Tasks</Text>
+        <View>
+        <FlatList
+        data={taskArray}
+        renderItem={
+          ({item})=>{
+            return(
+              <TaskCard  text={item.text}  />
+
+            );
+          }
+        }
+        />
+        </View>
+      </View>
+      
+      
+        <KeyboardAvoidingView style={styles.footer}>
+          <View style={styles.inputView}>
+            <TextInput style={styles.placeholder}
+            placeholder='Type Here' onChangeText={text=> setTask({
+              key: key ,
+              text:text
+
+            })} value={task || ''}
+            />
+          </View>
+          <View>
+            <TouchableOpacity onPress={()=> addTask()}>
+              <Text style={styles.button}>
+                Add
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+      
+    
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:10,
+    backgroundColor: '#385d8f',
+    justifyContent:"space-between",
+    alignItems:"center"
+    
   },
+ 
+  headerText:{
+    
+    color:"white",
+    alignItems:'center',
+    fontSize: 30,
+    fontWeight: "bold",
+    padding:30
+  },
+  footer:{
+    position:"absolute",
+    marginHorizontal:30,
+    padding:10,
+    borderRadius:10,
+    backgroundColor:"white",
+    bottom:20,
+    width:"100%",
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:'center',
+    
+  },
+  inputView:{
+
+    padding:15,
+    backgroundColor:"white",
+    borderRadius:60,
+    borderColor:"#385d8f",
+    borderWidth:1,
+    width:230,
+    height:50
+  },
+  button:{
+    padding:10,
+    paddingHorizontal:15,
+    borderRadius:30,
+    backgroundColor:"#385d8f",
+    fontSize:20,
+    color:"white"
+  }
 });
---
+++ TaslCard.js
@@ -0,0 +1,64 @@
+//import CheckBox from '@react-native-community/checkbox';
+import React, { useState } from 'react';
+import {Text,StyleSheet,View, TouchableOpacity} from "react-native";
+const TaskCard =(props)=>{
+
+
+const Deletetask=()=>{
+    console.log("deleted");
+}
+    return(
+        <View style={styles.card}>
+            <View style={styles.textArea}>
+            {/* <CheckBox
+              value={state}
+              onValueChange={value =>
+                setState({
+                    ...checkState,
+                  state: value,
+                })
+              }
+            /> */}
+            <TouchableOpacity style={styles.checkBox}></TouchableOpacity>
+            <Text style={{maxWidth:"75%"}}>{props.text}</Text>
+
+            </View>
+            <View>
+                <TouchableOpacity onPress={()=> Deletetask()}>
+                    <Text style={{color:"red"}}>Delete</Text>
+                </TouchableOpacity>
+            </View>
+            
+        </View>
+    );
+
+}
+
+const styles= StyleSheet.create({
+    card:{
+        width:"100%",
+        backgroundColor:"white",
+        padding:10,
+        borderRadius:10,
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"space-between",
+        marginBottom:20
+    },
+    textArea:{
+        flexDirection:"row",
+        alignItems:"center",
+        flexWrap:"wrap"
+    },
+    checkBox:{
+        height:20,
+        width:20,
+        borderColor:"green",
+        borderWidth:1,
+        marginRight:20
+
+    }
+
+});
+
+export default TaskCard;
